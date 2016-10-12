da1b4aa606b7c5fb457498a12b676e85fcc2c36c
wang stephen <551619855@qq.com>
2016-10-12 10:20:07 +0800

    tcp_server_run_well

--- script/tcp_sever.py
+++ script/tcp_sever.py
@@ -3,6 +3,8 @@
 
 from socket import *
 from time import ctime
+import time
+
 
 HOST = ''
 PORT = 6000
@@ -12,6 +14,11 @@ ADDR = (HOST, PORT)
 tcpSerSock = socket(AF_INET, SOCK_STREAM)
 tcpSerSock.bind(ADDR)
 tcpSerSock.listen(5)
+
+t_s = time.localtime()
+file_name = 'LOG_{0}_{1}_{2}_{3}_{4}_{5}.data'.format(t_s.tm_year,t_s.tm_mon,t_s.tm_mday,
+                                                                  t_s.tm_hour,t_s.tm_min,t_s.tm_sec)
+f_handle = open(file_name,'w')
 
 while True:
     print 'waiting for connection...'
@@ -24,7 +31,13 @@ while True:
             break
         #tcpCliSock.send('[%s] %s' %(ctime(), data))
         print (data)
+        f_handle.write(data)
     print("----------------------------------------------------")
+    f_handle.close()
+    t_s = time.localtime()
+    file_name = 'LOG_{0}_{1}_{2}_{3}_{4}_{5}.data'.format(t_s.tm_year, t_s.tm_mon, t_s.tm_mday,
+                                                           t_s.tm_hour, t_s.tm_min, t_s.tm_sec)
+    f_handle = open(file_name, 'w')
 tcpCliSock.close()
 
 tcpSerSock.close()
